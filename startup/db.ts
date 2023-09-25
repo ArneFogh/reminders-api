e4ab6df5dfa053f71430bbbd863e028efe3b10cf
arnefogh <andreasfogh7@gmail.com>
2023-09-25 15:23:26 +0200

    f

--- startup/db.ts
+++ startup/db.ts
@@ -1,10 +1,20 @@
-import mongoose from "mongoose";
-import config from "config";
+import mongoose from 'mongoose';
+import config from 'config';
 
 export default () => {
-  const db: string = config.get("MONGO_URI");
+  const db: string = config.get('MONGO_URI');
+  
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true, // Enable SSL as it's required by Azure
+    retryWrites: false, // Disable retryWrites as it's not supported by Azure Cosmos DB
+    maxIdleTimeMS: 120000, // You can set this according to your needs
+    appName: '@arnesreminderappfrontend@' // Optional, but good for identifying the app in logs
+  };
+
   mongoose
-    .connect(db)
+    .connect(db, options)
     .then(() => console.log(`Connected to ${db}...`))
-    .catch((err) => console.error("Could not connect to MongoDB...", err));
+    .catch((err) => console.error('Could not connect to MongoDB...', err));
 };
